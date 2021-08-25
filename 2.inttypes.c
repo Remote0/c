30720d1524c21b7ba3680c995412029a449eceb7
MyoMyoo <renboyu2333@gmail.com>
2021-08-25 16:09:27 +0800

    first commit

---
+++ 2.inttypes.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 2.inttypes.c
+	> Author: Boyu Ren
+	> Mail: renboyu2333@gmail.com
+	> Created Time: Wed 25 Aug 2021 03:02:30 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+
+    printf("%" "s","hai" "zei" "\n");
+    return 0;
+
+}
